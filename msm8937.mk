763928bd9ef209d49ea1fe5a6d6f65b0a49c43c7
Han Wang <416810799@qq.com>
2019-09-03 11:44:03 +0200

    msm8937-common: Add libnl back to PRODUCT_PACKAGES
     * Though this is a shared library built with wlan HAL, but with VNDK
       current it is in /system/lib(64)/vndk/ and cannot be used for WFD
       which locates in /system.
    
    Change-Id: I1a360be66a2523dcf43d401b933a4c6b2678242d

--- msm8937.mk
+++ msm8937.mk
@@ -384,5 +384,8 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
+PRODUCT_PACKAGES += \
+    libnl
+
 PRODUCT_BOOT_JARS += \
     WfdCommon
