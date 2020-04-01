26043c21ca809e4f0848333f662662981d7eda9d
MD Raza <37952910+raza231198@users.noreply.github.com>
2020-04-01 09:54:29 +0530

    msm8937-common: Build More Camera Packages

--- msm8937.mk
+++ msm8937.mk
@@ -109,11 +109,18 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    Snap
+
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.msm8937 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    libmmjpeg_interface \
     libmm-qcamera \
-    Snap
+    android.frameworks.displayservice@1.0
 
 # Configstore
 PRODUCT_PACKAGES += \
