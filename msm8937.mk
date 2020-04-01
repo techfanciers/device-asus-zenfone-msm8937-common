ca15c1c5bfbe57468bc3a67008735c08095bb4a0
MD Raza <raza231198@gmail.com>
2020-04-01 06:49:33 +0530

    msm8937-common: Define VENDOR_PATH
    Signed-off-by: MD Raza <raza231198@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -15,7 +15,7 @@
 #
 
 $(call inherit-product, vendor/asus/msm8937-common/msm8937-common-vendor.mk)
-
+VENDOR_PATH = device/asus/msm8937-common
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(VENDOR_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(VENDOR_PATH)/overlay-lineage
